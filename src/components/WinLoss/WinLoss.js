9317051535ad62567200400c7c17b0ad6ea9165a
ChrisCox12 <chriscox9856@gmail.com>
2022-01-11 21:37:57 -0500

    some more css

--- src/components/WinLoss/WinLoss.js
+++ src/components/WinLoss/WinLoss.js
@@ -8,15 +8,51 @@ function WinLoss({ userPick, setPicked, computerPick, result }) {
     const [win, setWin] = useState(false);
     const [draw, setDraw] = useState(false);
     const [loss, setLoss] = useState(false);
-
     
     useEffect(() => {
+        let uP = null;
+        let cP = null;
+        const userPicksBackg = document.querySelector('.user_picks-backg');
+        const computerPicksBackg = document.querySelector('.computer_picks-backg');
+
+        switch(userPick) {
+            case 'paper':
+                uP = document.querySelector('.results-screen__picks__user__paper');
+                break;
+            case 'scissors':
+                uP = document.querySelector('.results-screen__picks__user__scissors');
+                break;
+            case 'rock':
+                uP = document.querySelector('.results-screen__picks__user__rock');
+                break;
+            default:
+                break;
+        }
+
+        switch(computerPick) {
+            case 'paper':
+                cP = document.querySelector('.results-screen__picks__computer__paper');
+                break;
+            case 'scissors':
+                cP = document.querySelector('.results-screen__picks__computer__scissors');
+                break;
+            case 'rock':
+                cP = document.querySelector('.results-screen__picks__computer__rock');
+                break;
+            default:
+                break;
+        }
+
         switch(result) {
             case 'win':
                 setWin(true);
+                uP.classList.add('winner');
+                userPicksBackg.classList.remove('noshow');
                 break;
             case 'loss':
                 setLoss(true);
+                cP.classList.add('winner');
+                computerPicksBackg.classList.remove('noshow');
                 break;
             case 'draw':
                 setDraw(true);
@@ -24,34 +60,35 @@ function WinLoss({ userPick, setPicked, computerPick, result }) {
             default:
                 break;
         }
-    }, [result])
+    }, [result, userPick, computerPick])
     
 
     return (
         <div className='results-screen'>
-            <div className='results-screen__picks flex align-center justify-space-between'>
+            <div className='results-screen__picks flex align-center'>
                 <div className='results-screen__picks__user flex flex-column align-center justify-center'>
                     {userPick === 'paper' &&
                         <div className='results-screen__picks__user__paper flex justify-center align-center'>
                             <div className='flex justify-center align-center'>
-                                <img src={paper} alt='paper' />
+                                <img  className='rps-icon' src={paper} alt='paper' />
                             </div>
                         </div>
                     }
                     {userPick === 'scissors' &&
                         <div className='results-screen__picks__user__scissors flex justify-center align-center'>
                             <div className='flex justify-center align-center'>
-                                <img src={scissors} alt='scissors' />
+                                <img  className='rps-icon' src={scissors} alt='scissors' />
                             </div>
                         </div>
                     }
                     {userPick === 'rock' &&
                         <div className='results-screen__picks__user__rock flex justify-center align-center'>
                             <div className='flex justify-center align-center'>
-                                <img src={rock} alt='rock' />
+                                <img  className='rps-icon' src={rock} alt='rock' />
                             </div>
                         </div>
                     }
+                    <div className='user_picks-backg noshow'></div>
                     <p>YOU PICKED</p>
                 </div>
 
@@ -59,40 +96,41 @@ function WinLoss({ userPick, setPicked, computerPick, result }) {
                     {computerPick === 'paper' &&
                         <div className='results-screen__picks__computer__paper flex justify-center align-center'>
                             <div className='flex justify-center align-center'>
-                                <img src={paper} alt='paper' />
+                                <img  className='rps-icon' src={paper} alt='paper' />
                             </div>
                         </div>
                     }
                     {computerPick === 'scissors' &&
                         <div className='results-screen__picks__computer__scissors flex justify-center align-center'>
                             <div className='flex justify-center align-center'>
-                                <img src={scissors} alt='scissors' />
+                                <img  className='rps-icon' src={scissors} alt='scissors' />
                             </div>
                         </div>
                     }
                     {computerPick === 'rock' &&
                         <div className='results-screen__picks__computer__rock flex justify-center align-center'>
                             <div className='flex justify-center align-center'>
-                                <img src={rock} alt='rock' />
+                                <img  className='rps-icon' src={rock} alt='rock' />
                             </div>
                         </div>
                     }
+                    <div className='computer_picks-backg noshow'></div>
                     <p>THE HOUSE PICKED</p>
                 </div>
             </div>
-            {win && 
-                <p className='results-screen__win-loss-draw'>YOU WIN</p>
-            }
-            
-            {loss && 
-                <p className='results-screen__win-loss-draw'>YOU LOSE</p>
-            }
-
-            {draw && 
-                <p className='results-screen__win-loss-draw'>TIE</p>
-            }
-
-            <button className='results-screen__play-again-button' onClick={() => setPicked(false)}>PLAY AGAIN</button>
+            <div className="results-screen__rapa">
+                {win &&
+                    <p className='results-screen__win-loss-draw'>YOU WIN</p>
+                }
+                
+                {loss &&
+                    <p className='results-screen__win-loss-draw'>YOU LOSE</p>
+                }
+                {draw &&
+                    <p className='results-screen__win-loss-draw'>TIE</p>
+                }
+                <button className='results-screen__play-again-button' onClick={() => setPicked(false)}>PLAY AGAIN</button>
+            </div>
         </div>
     )
 }
